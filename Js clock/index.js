e3816afdde59538d739a042cd529d45ee392aefa
Kawsar Ahammed <115506566+thebegjoker@users.noreply.github.com>
2023-03-13 10:15:44 -0700

    Add files via upload

---
+++ Js clock/index.js
@@ -0,0 +1,23 @@
+setInterval(clock,100);
+
+
+let secondHand = document.querySelector('.second')
+let minuteHand = document.querySelector('.minute')
+let hourdHand = document.querySelector('.hour')
+
+
+function clock(){
+    const date = new Date()
+
+    const second = date.getSeconds()/60
+    const minute = (second+date.getMinutes())/60
+    const hour = (minute+date.getHours())/12
+
+    setRoted(secondHand,second )
+    setRoted(minuteHand,minute )
+    setRoted(hourdHand,hour )
+}
+
+function setRoted(el,rot){
+    el.style.setProperty('--roted', rot*360)
+}
